875f58faae8772d3157297d897f0a5198fa85d74
Ikenna Iheanaetu <ikennae120@gmail.com>
2023-05-30 00:47:41 +0100

    Fetched the name value from the async storage and stored it in a user state variable called user all done in the todo.js file in the screens folder

--- screens/Todo.js
+++ screens/Todo.js
@@ -1,10 +1,33 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 import { View, Text } from 'react-native'
-import React from 'react'
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Todo({ onLayout }) {
+    const [ user, setUser ] = useState('')
+
+    useEffect(() => {
+        
+const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('user')
+      if(value !== null) {
+        // value previously stored
+        const name = JSON.parse(value)
+        console.log(value);
+        setUser(name.name)
+      }
+    } catch(e) {
+      // error reading value
+    }
+  }
+
+  getData()
+    }, [])
+
   return (
     <View style={{flex: 1, justifyContent: "center", alignItems: "center"}} onLayout={onLayout}>
-      <Text>Todo Page</Text>
+      <Text>{ user } Todo Page</Text>
     </View>
   )
-}
+} 
